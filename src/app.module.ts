5734b2e9b0bc3b9e84fa4ded1854eccc25249b04
Gabriele <gabriele.i.sousa@aluno.senai.br>
2026-08-28 16:19:07 -0300

    feat: implement basic structure and crud endpoints of menu.

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,11 @@ import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { TablesModule } from './tables/tables.module';
 import { OrdersModule } from './orders/orders.module';
-import { ReservationsModule } from './reservations/reservations.module'
+import { ReservationsModule } from './reservations/reservations.module';
+import { MenuModule } from './menu/menu.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, OrdersModule, ReservationsModule,ConfigModule.forRoot({ isGlobal: true }), TablesModule],
+  imports: [PrismaModule, AuthModule, OrdersModule, ReservationsModule, MenuModule,ConfigModule.forRoot({ isGlobal: true }), TablesModule],
   controllers: [AppController],
   providers: [AppService],
 })
